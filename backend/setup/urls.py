eff3e17088c27d1ccf4e07a64a1dd3bb69be0eb5
Marvyn Mesquita <31486769+marvynmesquita@users.noreply.github.com>
2024-10-09 08:27:55 -0300

    aula 08/10/2024

--- backend/setup/urls.py
+++ backend/setup/urls.py
@@ -19,11 +19,17 @@ from django.contrib import admin
 from django.urls import path
 from todos.views import TodoListView
 from todos.views import TodoCreateView
+from todos.views import TodoUpdateView
 
-urlpatterns = [path("admin/", admin.site.urls),
- path("", TodoListView.as_view()),
- path("create", TodoCreateView.as_view())]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", TodoListView.as_view()),
+    path("create", TodoCreateView.as_view()),
+]
 
-urlpatterns = [path("admin/", admin.site.urls),
- path("", TodoListView.as_view(), name="todo_list"),
- path("create", TodoCreateView.as_view(), name="todo_create")]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", TodoListView.as_view(), name="todo_list"),
+    path("create", TodoCreateView.as_view(), name="todo_create"),
+    path("update/<int:pk>", TodoUpdateView.as_view(), name="todo_update"),
+]
--- backend/todos/views.py
+++ backend/todos/views.py
@@ -1,19 +1,28 @@
 from django.shortcuts import render
+
 # Create your views here.
 from django.http import HttpResponse
 from .models import Todo
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView
 from django.urls import reverse_lazy
 
 
 class TodoListView(ListView):
     model = Todo
 
-class TodoCreateView(CreateView):
- model = Todo
- fields = ["title", "deadline"]
 
 class TodoCreateView(CreateView):
- model = Todo
- fields = ["title", "deadline"]
- success_url = reverse_lazy("todo_list")
+    model = Todo
+    fields = ["title", "deadline"]
+
+
+class TodoCreateView(CreateView):
+    model = Todo
+    fields = ["title", "deadline"]
+    success_url = reverse_lazy("todo_list")
+
+
+class TodoUpdateView(UpdateView):
+    model = Todo
+    fields = ["title", "deadline"]
+    success_url = reverse_lazy("todo_list")
